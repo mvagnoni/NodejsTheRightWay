fda66d70c9cef870e92e3ae1b53fa23e6cc116c0
Matt Vagnoni <matt.vagnoni@gmail.com>
2015-02-01 15:07:17 -0600

    Finished 4th version where we parse and use events.

---
+++ chapter2/watcher4-spawn-parse.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const fs = require('fs');
+const filename = process.argv[2];
+const spawn = require('child_process').spawn;
+
+if(!filename) { 
+	throw Error("A file to watch must be specified!");
+}
+fs.exists(filename, function(exists) {
+	if(exists) {
+		fs.watch(filename, function() {
+			let 
+			  ls = spawn('ls', ['-lh', filename]),
+			  output = '';
+			ls.stdout.on('data', function(chunk) {
+				output += chunk.toString();
+			});
+			ls.on('close', function() {
+				let parts = output.split(/\s+/);
+				console.dir([parts[0], parts[4], parts[8]]);
+			});
+		});
+		console.log("Now watching '" + filename + "' for changes...");
+	}
+	else {
+		throw Error("A file to watch must be specified!");
+	}
+});
+
+
